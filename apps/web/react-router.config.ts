42b0e9431488c2314800b212efa9d412b8bfded0
nathancolosimo <nathancolosimo@gmail.com>
2025-10-23 10:42:57 -0400

    Added vercel preset config

--- apps/web/react-router.config.ts
+++ apps/web/react-router.config.ts
@@ -1,6 +1,8 @@
 import type { Config } from "@react-router/dev/config";
+import { vercelPreset } from "@vercel/react-router/vite";
 
 export default {
   ssr: false,
   appDirectory: "src",
+  presets: [vercelPreset()],
 } satisfies Config;
